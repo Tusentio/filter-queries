f601275175d383fe5e2ea58459492adb88c7acdd
Keve1227 <killenlillen02@gmail.com>
2021-02-19 10:42:25 +0100

    Make it better

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
 /**
- * @param {Object} [options]
- * @param {function (Object, Object, function () : void) : void} [options.violationHandler]
- * @returns {function (...string) : function (Object, Object, function () : void) : void}
+ * @param {any} [options]
+ * @param {function (any, any, function () : void) : void} [options.violationHandler]
+ * @returns {function (...string) : function (any, any, function () : void) : void}
  */
 module.exports = (options = {}) => {
     const {
         violationHandler = (_, res) => {
-            res.status(400).send("Bad Request");
+            return res.sendStatus(400);
         },
     } = options;
 
     return function (...required) {
         return (req, res, next) => {
-            Object.keys(req.query).forEach((key) => {
-                if ((req.query[key]?.length || 0) <= 0) {
-                    delete req.query[key];
-                }
-            });
+            const queryKeys = new Set(Object.getOwnPropertyNames(req.query));
+            const missingKey = required.find((key) => !queryKeys.has(key));
 
-            for (let key of required) {
-                if (req.query[key] === undefined) {
-                    return violationHandler(req, res, next);
-                }
+            if (missingKey !== undefined) {
+                return void violationHandler(req, res, next);
             }
 
             next();
